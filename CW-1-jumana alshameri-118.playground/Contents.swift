a9ac3ac2c7f9cfb6273c13767156e1dda4a9ab67
jumana-alshameri <66526830+jumana-alshameri@users.noreply.github.com>
2020-06-13 19:16:15 +0300

    part 1 done

---
+++ CW-1-jumana alshameri-118.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let name = "jumana"
+var hobby1 = "horse riding"
+var hobby2 = "drawing"
+var age = 18
+var height = 153
+print("my name is \(name), \(hobby1), \(hobby2), \(age), \(height)")
