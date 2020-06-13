6e43516d7dc8ffd7e570f63cdac9c3ce65bd16e9
jumana-alshameri <66526830+jumana-alshameri@users.noreply.github.com>
2020-06-13 20:27:56 +0300

    part1&part2 done

--- CW-1-jumana alshameri-118.playground/Contents.swift
+++ CW-1-jumana alshameri-118.playground/Contents.swift
@@ -1,8 +1,27 @@
 import UIKit
-
+// part 1
 let name = "jumana"
 var hobby1 = "horse riding"
 var hobby2 = "drawing"
 var age = 18
-var height = 153
+var height = 1.53
 print("my name is \(name), \(hobby1), \(hobby2), \(age), \(height)")
+
+//  part 2
+let friendName = "sara"
+var friendAge = 17
+var friendHeight = 1.60
+var friendHobby1 = "swimming"
+var friendHobby2 = "drawing"
+
+name != friendName
+age > friendAge
+height < friendHeight
+hobby1 != friendHobby1
+hobby2 == friendHobby2
+
+name == friendName
+age < friendAge
+height > friendHeight
+hobby1 == friendHobby1
+hobby2 != friendHobby2
